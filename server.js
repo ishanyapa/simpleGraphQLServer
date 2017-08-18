28c16833562a90c2b47a5175eca6aff631f66d6e
Ishan Yapa <ishan.adithya@pearson.com>
2017-08-18 16:00:47 +0530

    Schema added

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const express = require('express');
-//const expressGraphQL = require('express-graphql');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
 
 const port = process.env.PORT || 4889;
-
 const app = express();
+
+app.use('/graphql',expressGraphQL({
+    schema:schema,
+    graphiql:true
+}));
 
 app.listen(port,(err) => {
     if(err){
